4340c39260ec6b0e60e75b1166a7ea5e3e6b9c2b
sudha-kuppusamy <68539313+sudha-kuppusamy@users.noreply.github.com>
2020-07-21 16:43:12 +0530

    Fix typo error

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,10 +1,13 @@
 
-bool compare(float param, float minValue, float maxValue)
+static bool compare(float param, float minValue, float maxValue);
+static bool compare(float param, float minValue);
+
+static bool compare(float param, float minValue, float maxValue)
 {
   return (param < minValue) || (param > maxValue);
 }
 
-book compare(float param, float minValue)
+static bool compare(float param, float minValue)
 {
   return (param < minValue)
 }
@@ -14,11 +17,11 @@ bool vitalsAreOk(float bpm, float spo2, float respRate) {
   ret = compare(bpm, 70, 150);
   if(!ret) return ret;
   
-  ret = compare (spo2, 80)
-  if(!ret) return false;
+  ret = compare (spo2, 80);
+  if(!ret) return ret;
   
-  ret = compare(erspRate, 30, 60);
-  if(!ret) return false;
+  ret = compare(respRate, 30, 60);
+  if(!ret) return ret;
   
-   return true;
+   return ret;
 }
