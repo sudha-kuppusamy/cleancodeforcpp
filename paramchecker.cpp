2679204d16f1ef511c2e06ceca134e364ad95502
sudha-kuppusamy <68539313+sudha-kuppusamy@users.noreply.github.com>
2020-07-21 22:17:19 +0530

    use variable number of arguments

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,20 +1,34 @@
 
-static bool compare(float param, float minValue, float maxValue);
-static bool compare(float param, float minValue);
+// static bool compare(float param, float minValue, float maxValue);
+// static bool compare(float param, float minValue);
 
-static bool compare(float param, float minValue, float maxValue)
+// static bool compare(float param, float minValue, float maxValue)
+// {
+//   return (param < minValue) || (param > maxValue);
+// }
+
+// static bool compare(float param, float minValue)
+// {
+//   return (param < minValue);
+// }
+
+template<typename T>
+bool vitalsAreOk(T var)
 {
-  return (param < minValue) || (param > maxValue);
-}
-
-static bool compare(float param, float minValue)
-{
-  return (param < minValue);
-}
-
-bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  if(compare(bpm, 70, 150) || compare (spo2, 80) || compare(respRate, 30, 60))
-    return true;
+  if ((var < 80) || (var > 60))
+    return false;
   else
-   return false;
+    return true;
+
+template<typename T, typename... Args>
+bool vitalsAreOk(T var1, Args... var2) {
+  if(vitalAreOk(var1)   
+    return vitalAreOk(var2...);
+  else
+    return false;
 }
+//   if(compare(bpm, 70, 150) || compare (spo2, 80) || compare(respRate, 30, 60))
+//     return true;
+//   else
+//    return false;
+// }
