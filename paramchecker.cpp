4e877301a59392c9120eac9b95bf431f21a2c23e
sudha-kuppusamy <68539313+sudha-kuppusamy@users.noreply.github.com>
2020-07-21 16:36:18 +0530

    Add generic function to compare values

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,11 +1,24 @@
 
+bool compare(float param, float minValue, float maxValue)
+{
+  return (param < minValue) || (param > maxValue);
+}
+
+book compare(float param, float minValue)
+{
+  return (param < minValue)
+}
+
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  if(bpm < 70 || bpm > 150) {
-    return false;
-  } else if(spo2 < 80) {
-    return false;
-  } else if(respRate < 30 || respRate > 60) {
-    return false;
-  }
-  return true;
+  bool ret = true;
+  ret = compare(bpm, 70, 150);
+  if(!ret) return ret;
+  
+  ret = compare (spo2, 80)
+  if(!ret) return false;
+  
+  ret = compare(erspRate, 30, 60);
+  if(!ret) return false;
+  
+   return true;
 }
