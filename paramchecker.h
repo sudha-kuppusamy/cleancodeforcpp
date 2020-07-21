7a7c6c5ba4145b83398b3c575ded7933ebb7c1df
sudha-kuppusamy <68539313+sudha-kuppusamy@users.noreply.github.com>
2020-07-21 22:33:53 +0530

    Update paramchecker.h

--- paramchecker.h
+++ paramchecker.h
@@ -1,2 +1,18 @@
 
-bool vitalsAreOk(float bpm, float spo2, float respRate);
+//bool vitalsAreOk(float bpm, float spo2, float respRate);
+
+template<typename T>
+bool vitalsAreOk(T var)
+{
+  if ((var < 80) || (var > 60))
+    return false;
+  else
+    return true;
+}
+template<typename T, typename... Args>
+bool vitalsAreOk(T var1, Args... var2) {
+  if(vitalAreOk(var1)  ) 
+    return vitalsAreOk(var2...);
+  else
+    return false;
+}
