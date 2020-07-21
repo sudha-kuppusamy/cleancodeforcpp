e1c2852b6aafdb30f49501908a8639cc7fb6d19d
sudha-kuppusamy <68539313+sudha-kuppusamy@users.noreply.github.com>
2020-07-21 22:34:59 +0530

    comment all code

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -12,21 +12,21 @@
 //   return (param < minValue);
 // }
 
-template<typename T>
-bool vitalsAreOk(T var)
-{
-  if ((var < 80) || (var > 60))
-    return false;
-  else
-    return true;
-}
-template<typename T, typename... Args>
-bool vitalsAreOk(T var1, Args... var2) {
-  if(vitalAreOk(var1)  ) 
-    return vitalsAreOk(var2...);
-  else
-    return false;
-}
+// template<typename T>
+// bool vitalsAreOk(T var)
+// {
+//   if ((var < 80) || (var > 60))
+//     return false;
+//   else
+//     return true;
+// }
+// template<typename T, typename... Args>
+// bool vitalsAreOk(T var1, Args... var2) {
+//   if(vitalAreOk(var1)  ) 
+//     return vitalsAreOk(var2...);
+//   else
+//     return false;
+// }
 //   if(compare(bpm, 70, 150) || compare (spo2, 80) || compare(respRate, 30, 60))
 //     return true;
 //   else
