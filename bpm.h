d2ebe584bbb02fce44577672c8fbbaea91ec6efb
sudha-kuppusamy <68539313+sudha-kuppusamy@users.noreply.github.com>
2020-07-22 16:58:47 +0530

    remove implementation from header file

--- bpm.h
+++ bpm.h
@@ -3,14 +3,6 @@
 class bpm : public IVital
 {
   bpm();
-  ~bpm();
-  RegisterVital(char* name, float &minValue, float &maxValue)
-  {
-    if(name != nullptr)
-    {
-        strcpy(name, "bpm");
-        minValue = 70;
-        maxValue = 150;
-    }
-  }
+  virtual ~bpm();
+  virtual RegisterVital(char* name, float &minValue, float &maxValue);
 }
