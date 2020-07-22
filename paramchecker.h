25b3f05ec7a66d89cee3861221d7449a956b97c3
sudha-kuppusamy <68539313+sudha-kuppusamy@users.noreply.github.com>
2020-07-22 17:33:38 +0530

    Add list of vitals and its min, max values in map

--- paramchecker.h
+++ paramchecker.h
@@ -1,2 +1,8 @@
+std::map < std::string, <float, float>) vitalsList =
+{
+  {"bpm", {70, 150}},
+  {"spo2", {80, 100}},
+  {"respRate", {30, 60}}
+}
 
 bool vitalsAreOk(float bpm, float spo2, float respRate);
