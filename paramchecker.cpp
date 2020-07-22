5f21cc83093e6a03d480aff4dfcca28dbe25fa82
sudha-kuppusamy <68539313+sudha-kuppusamy@users.noreply.github.com>
2020-07-22 16:01:05 +0530

    Revert the changes

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,34 +1,11 @@
-
-// static bool compare(float param, float minValue, float maxValue);
-// static bool compare(float param, float minValue);
-
-// static bool compare(float param, float minValue, float maxValue)
-// {
-//   return (param < minValue) || (param > maxValue);
-// }
-
-// static bool compare(float param, float minValue)
-// {
-//   return (param < minValue);
-// }
-
-// template<typename T>
-// bool vitalsAreOk(T var)
-// {
-//   if ((var < 80) || (var > 60))
-//     return false;
-//   else
-//     return true;
-// }
-// template<typename T, typename... Args>
-// bool vitalsAreOk(T var1, Args... var2) {
-//   if(vitalAreOk(var1)  ) 
-//     return vitalsAreOk(var2...);
-//   else
-//     return false;
-// }
-//   if(compare(bpm, 70, 150) || compare (spo2, 80) || compare(respRate, 30, 60))
-//     return true;
-//   else
-//    return false;
-// }
+bool vitalsAreOk(float bpm, float spo2, float respRate) {
+  int i;
+  if(bpm < 70 || bpm > 150) {
+    return false;
+  } else if(spo2 < 80) {
+    return false;
+  } else if(respRate < 30 || respRate > 60) {
+    return false;
+  }
+  return true;
+}
